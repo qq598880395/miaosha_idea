e1cd5b3cb5a0b267dcb2e7dce8174ca970a6eb38
qq598880395 <598880395@qq.com>
2019-09-04 00:23:05 +0800

    秒杀功能完成

--- src/main/java/com/imooc/miaosha/controller/GoodsController.java
+++ src/main/java/com/imooc/miaosha/controller/GoodsController.java
@@ -5,21 +5,21 @@ import com.imooc.miaosha.domain.MiaoshaUser;
 import com.imooc.miaosha.domain.User;
 import com.imooc.miaosha.redis.RedisService;
 import com.imooc.miaosha.result.Result;
+import com.imooc.miaosha.service.GoodsService;
 import com.imooc.miaosha.service.MiaoshaUserService;
+import com.imooc.miaosha.vo.GoodsVo;
 import com.imooc.miaosha.vo.LoginVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
@@ -32,11 +32,42 @@ public class GoodsController {
     MiaoshaUserService userService;
     @Autowired
     RedisService redisService;
+    @Autowired
+    GoodsService goodsService;
 
     @RequestMapping("/to_list")
     public String toList(Model model, MiaoshaUser user) {
         model.addAttribute("user",user);
+        List<GoodsVo> goodsList=goodsService.listGoodsVo();
+        model.addAttribute("goodsList",goodsList);
+
+
         return "goods_list";
+    }
+    @RequestMapping("/to_detail/{goodsId}")
+    public String detail(Model model, MiaoshaUser user, @PathVariable("goodsId")long goodsId) {//@pathvariable用来获取路径中的{goodsId}参数
+        model.addAttribute("user",user);
+
+        GoodsVo goods=goodsService.getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+        int miaoshaStatus = 0;
+        int remainSeconds = 0;
+        if(now < startAt){//秒杀还没开始，倒计时
+            miaoshaStatus=0;
+            remainSeconds = (int)((startAt-now)/1000);
+        }else if(now>endAt){//秒杀已经结束
+            miaoshaStatus=2;
+            remainSeconds=-1;
+        }else{//秒杀进行中
+            miaoshaStatus=1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("miaoshaStatus",miaoshaStatus);
+        model.addAttribute("remainSeconds",remainSeconds);
+        return "goods_detail";
     }
 
 
